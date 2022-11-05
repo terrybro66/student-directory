b374cc51d25e904054d63bcaf4882a51bfb223d1
terrybro66 <terrybro61@gmail.com>
2022-11-05 18:22:16 +0000

    feature: add user input to create student list

--- directory.rb
+++ directory.rb
@@ -1,20 +1,19 @@
-students = [
-  { name: "Dr. Hannibal Lecter", cohort: :november },
-  { name: "Darth Vader", cohort: :november },
-  { name: "Nurse Ratched", cohort: :november },
-  { name: "Michael Corleone", cohort: :november },
-  { name: "Alex DeLarge", cohort: :november },
-  { name: "The Wicked Witch of the West", cohort: :november },
-  { name: "Terminator", cohort: :november },
-  { name: "Freddy Krueger", cohort: :november },
-  { name: "The Joker", cohort: :november },
-  { name: "Joffrey Baratheon", cohort: :november },
-  { name: "Norman Bates", cohort: :november },
-]
+def input_students
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
+  students = []
+  name = gets.chomp
+  until name.empty?
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts 'The students of Villains Academy'
+  puts '-------------'
 end
 
 def print(students)
@@ -27,7 +26,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-#nothing happens until we call the methods
+# nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
